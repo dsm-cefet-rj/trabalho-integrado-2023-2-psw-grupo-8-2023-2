195afb0da2a0543f82be9a75809b10ee001825d9
Danilo011 <danilofreire.h@gmail.com>
2023-09-09 20:50:58 -0300

    Detalhes do Produto
    Funcionando a interatividade do id pela url, mas ainda inacabado

--- front-end/src/pages/produto/productDetails.js
+++ front-end/src/pages/produto/productDetails.js
@@ -1,65 +1,22 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-// import { reducer } from "../../store";
-// import { userSelector} from 'react-redux';
+import { PRODUCTS } from "../../data";
 
 export default function ProductDetails() {
   const { id } = useParams()
-  let productData = null
-  if (id == 1) {
-    productData = {
-      name: "T1",
-      price: 10,
-      image: require("../../images/tenis1.jpg"),
-    }
-
-  } else if (id == 2) {
-    productData = {
-      name: "T2",
-      price: 20,
-      image: require("../../images/tenis2.jpg"),
-    }
-  } else if (id == 3) {
-    productData = {
-      name: "T3",
-      price: 30,
-      image: require("../../images/tenis3.jpg"),
-    }
-  } else if (id == 4) {
-    productData = {
-      name: "T4",
-      price: 40,
-      image: require("../../images/tenis4.jpg"),
-    }
-  } else if (id == 5) {
-    productData = {
-      name: "T2",
-      price: 50,
-      image: require("../../images/tenis5.jpg"),
-    }
-  }
+  const idInt = parseInt(id, 10)
 
   return (<>
-    <h1>{productData.name}</h1>
-    <h2>O id do produto é {id}</h2>
-    {
-      productData != null ?
-        <>
-          <img src={productData.image} alt={productData.name} width="300" />
-          <h2>R${productData.price}</h2>
-        </>
-        : <> <h1>SEM DADOS</h1></>
-    }
+    <div className='product'>
+      <div className="image-container">
+        <img className="responsive-image" src={PRODUCTS[(idInt - 1)].img} fluid="true" responsive="true" alt="productImage" />
+      </div>
+      <div className='description'>
+        <p> <b> <h3>{PRODUCTS[(idInt - 1)].nome} </h3></b> </p>
+        <p> <h4>R${PRODUCTS[(idInt - 1)].preço}</h4> </p>
+        <p> <h4>Tipo: {PRODUCTS[(idInt - 1)].tipo}</h4> </p>
+        <p> <h4>Id: {PRODUCTS[(idInt - 1)].id}</h4> </p>
+      </div>
+    </div>
   </>)
-  // const dados = userSelector((state) => state.dados);
-  // return (
-  //     <div>
-  //       {dados.map((item) => (
-  //         <div key={item.id}>
-  //           <p>Nome: {item.name}</p>
-  //           <p>Preço: {item.price}</p>
-  //         </div>
-  //       ))}
-  //     </div>
-  //   );
 }
