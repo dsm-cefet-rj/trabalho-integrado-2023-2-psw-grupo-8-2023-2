bfb32003e19b8d15559488b6bc0ba70545b17a23
Danilo011 <danilofreire.h@gmail.com>
2023-09-15 20:21:50 -0300

    Detalhes do Produto
    Atualização na página dos detalhes do produto mas ainda não finalizada

--- front-end/src/data.js
+++ front-end/src/data.js
@@ -6,10 +6,10 @@ import tenis5Img from './images/tenis5.jpg'
 import tenis6Img from './images/tenis6.jpg'
 
 export const PRODUCTS = [
-    {tipo:"masculino", id: 1,nome:"Tênis Nike Sb Chron 2 Preto/Branco", preço: 199.99, img: tenis1Img },
-    {tipo:"infantil", id: 2,nome:"Air Jordan 1 Retro High", preço: 149.99, img: tenis2Img },
-    {tipo:"feminino", id: 3,nome:"Tênis Stan Smith - Branco adidas", preço: 179.99, img: tenis3Img },
-    {tipo:"masculino", id: 4,nome:"Tênis Nike Air Max SC", preço: 189.99, img: tenis4Img },
-    {tipo:"masculino", id: 5,nome:"Tênis Adidas Stan Smith", preço: 159.99, img: tenis5Img },
-    {tipo:"masculino", id: 6,nome:"Tênis Nike Shox", preço: 169.99, img: tenis6Img },
+    {tipo:"masculino", id: 1,nome:"Tênis Nike Sb Chron 2 Preto/Branco", preço: 199.99, img: tenis1Img, desc: "<Blablabla1>" },
+    {tipo:"infantil", id: 2,nome:"Air Jordan 1 Retro High", preço: 149.99, img: tenis2Img, desc: "Blablabla2"},
+    {tipo:"feminino", id: 3,nome:"Tênis Stan Smith - Branco adidas", preço: 179.99, img: tenis3Img, desc: "Blablabla3" },
+    {tipo:"masculino", id: 4,nome:"Tênis Nike Air Max SC", preço: 189.99, img: tenis4Img, desc: "Blablabla4" },
+    {tipo:"masculino", id: 5,nome:"Tênis Adidas Stan Smith", preço: 159.99, img: tenis5Img, desc: "Blablabla5" },
+    {tipo:"masculino", id: 6,nome:"Tênis Nike Shox", preço: 169.99, img: tenis6Img, desc: "Blablabla6" },
 ]
--- front-end/src/pages/produto/productDetails.js
+++ front-end/src/pages/produto/productDetails.js
@@ -1,30 +1,113 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { PRODUCTS } from "../../data";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import "../allProducts/allProducts.css"
-
+import { Container, Row, Col } from 'react-bootstrap';
+import Carousel from 'react-bootstrap/Carousel';
+import './productDetails.css';
 
 export default function ProductDetails() {
   const { id } = useParams()
-  const idInt = parseInt(id, 10)
+  const product = PRODUCTS.find((element) => element.id == id);
 
   return (<>
-  <br></br>
-  <Container fluid="true">
-    <div className='product'>
-      <div className="image-container">
-        <img className="responsive-image" src={PRODUCTS[(idInt - 1)].img} fluid="true" responsive="true" alt="productImage" />
+    <Container>
+      <h3><b>{product.nome}</b></h3>
+    </Container>
+
+    <Carousel data-bs-theme="dark">
+      <Carousel.Item>
+        <img
+          className="d-block w-25"
+          src={product.img} fluid="true"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-25"
+          src={product.img} fluid="true"
+          alt="Second slide"
+        />
+        <Carousel.Caption>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-25"
+          src={product.img} fluid="true"
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+
+    <Container fluid="true" className="ac">
+      <h3 className="justify-content-md-center align-items-center">Tamanhos</h3>
+      <Row>
+        <Col><button className="tamanho-button m-2">34</button></Col>
+        <Col><button className="tamanho-button m-2">35</button></Col>
+        <Col><button className="tamanho-button m-2">36</button></Col>
+      </Row>
+      <Row>
+        <Col><button className="tamanho-button m-2">39</button></Col>
+        <Col><button className="tamanho-button m-2">39,5</button></Col>
+        <Col><button className="tamanho-button m-2">40</button></Col>
+      </Row>
+      <Row>
+        <Col><button className="tamanho-button m-2">41</button></Col>
+        <Col><button className="tamanho-button m-2">41,5</button></Col>
+        <Col><button className="tamanho-button m-2">42</button></Col>
+      </Row>
+    </Container>
+
+    <Container fluid="true" className="ac">
+      <h3>Cores</h3>
+      <Row>
+        <Col><p>Preto</p>
+          <input type="button" id="black" /></Col>
+        <Col><p>Vermelho</p>
+          <input type="button" id="red" /></Col>
+        <Col><p>Verde</p>
+          <input type="button" id="green" /></Col>
+      </Row>
+      <Row>
+        <Col className=""><p>Azul</p>
+          <input type="button" id="blue" /></Col>
+        <Col><p>Branco</p>
+          <input type="button" id="white" /></Col>
+        <Col><p>Amarelo</p>
+          <input type="button" id="yellow" /></Col>
+      </Row>
+      <Row>
+        <Col><p>Rosa</p>
+          <input type="button" id="pink" /></Col>
+        <Col><p>Roxo</p>
+          <input type="button" id="purple" /></Col>
+        <Col><p>Laranja</p>
+          <input type="button" id="orange" /></Col>
+      </Row>
+    </Container>
+
+    <Container fluid="true">
+      <div class="container-fluid">
+        <div class="buy d-block">
+          <div class="text-center">
+            <button type="button">COMPRAR AGORA</button>
+            <button type="button">ADICIONAR AO CARRINHO</button>
+          </div>
+        </div>
       </div>
-      <div className='description'>
-        <p> <b> {PRODUCTS[(idInt - 1)].nome} </b> </p>
-        <p><h4>R${PRODUCTS[(idInt - 1)].preço}</h4></p>
-        <p><h4>Tipo: {PRODUCTS[(idInt - 1)].tipo}</h4></p>
-        <h4>Id: {PRODUCTS[(idInt - 1)].id}</h4>
+    </Container>
+
+
+    <Container fluid="true">
+      <div class="container-fluid">
+        <h3>Descrição do Produto</h3>
+        <p>{product.desc}</p>
       </div>
-    </div>
     </Container>
   </>)
 }
