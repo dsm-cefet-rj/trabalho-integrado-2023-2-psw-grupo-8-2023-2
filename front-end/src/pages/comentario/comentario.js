e8062d38c50f00e357dd2bc468288016c758d1af
Danilo011 <danilofreire.h@gmail.com>
2023-09-25 14:51:00 -0300

    Update comentario.js

--- front-end/src/pages/comentario/comentario.js
+++ front-end/src/pages/comentario/comentario.js
@@ -3,21 +3,32 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     comments: [],
-    comment: "",
-    value: 0,
+    ids: []
 };
 
 const commentsSlice = createSlice({
     name: "comment",
     initialState,
     reducers: {
-        addComment(state, action){
-            const newComment = action.payload;
-            state.comments.push(newComment);
+        addComment(state, action) {
+            const { productId, comment } = action.payload;
+            if (!state.comments[productId]) {
+                state.comments[productId] = [];
+            }
+            state.comments[productId].push(comment);
         },
+        removeComment: (state, action) => {
+            const { productId, index } = action.payload;
+            state.comments[productId].splice(index, 1);
+        },
+        // editComment: (state, action) => {
+        //     const { productId, index, editedComment } = action.payload;
+        //     console.log([action.payload])
+        //     state.comments[productId][index] = editedComment;
+        // },
     }
 });
 
 
 export default commentsSlice.reducer;
-export const { addComment , increment, decrement} = commentsSlice.actions;
+export const { addComment, removeComment,/* editComment */} = commentsSlice.actions;
